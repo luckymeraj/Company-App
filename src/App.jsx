a919238283e9e65eb1f604b21666b8c457b59fff
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-10-03 20:02:35 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 import Home from './Home';
 import About from './About';
 import Service from './Service';
 import Contact from './Contact';
+import Navbar from './Navbar';
 // import { Route, Router, Routes } from 'react-router-dom'
 import {
   BrowserRouter as Router,
@@ -12,16 +17,14 @@ function App() {
   return (
 
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/home' element={<Home/>} />
-
       <Route path='/about' element={<About/>} />
       <Route path='/service' element={<Service/>} />
       <Route path='/contact' element={<Contact/>} />
       <Route path='/*' element={<Home/>} />
-
-
 
 
     </Routes>
