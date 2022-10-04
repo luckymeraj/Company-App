c7637c1f5023dae6a630b348f2e5e8787b458922
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-10-04 09:34:44 +0530

    brand

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,10 +8,9 @@ const Navbar = () => {
                 <div className="row">
                     <div className="col-10 mx-auto">
 
-
                         <nav className="navbar navbar-expand-lg ">
                             <div className="container-fluid">
-                                <NavLink className="navbar-brand" to="/home">ThisShala.com</NavLink>
+                                <NavLink className="navbar-brand" to="/home">ThisShala<span>.com</span></NavLink>
 
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
