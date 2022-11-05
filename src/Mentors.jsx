fdf1c37ad33ad9736ff6787a6b244e7047de3982
Meraj Rizwan <meraj.rizwan9832@gmail.com>
2022-11-05 23:15:38 +0530

    999

--- src/Mentors.jsx
+++ src/Mentors.jsx
@@ -9,7 +9,7 @@ const Mentors = () => {
         (async () => {
             let data
             try {
-                const res = await fetch('https://randomuser.me/api/?results=99')
+                const res = await fetch('https://randomuser.me/api/?results=999')
                 data = (await res.json()).results
             } catch (error) {
                 console.log(error);
